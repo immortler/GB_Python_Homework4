22f51dfd4ae3cdd95d49eecec0222a124fb6acea
Aleksandr <bestprogreraxe@gmail.com>
2023-06-03 02:20:34 +0300

    Second commmit - solved task 22

--- Task22/main.py
+++ Task22/main.py
@@ -5,3 +5,15 @@
 пользователь вводит сами элементы множеств.
 """
 
+len_of_first_list = int(input('Enter length of first set of integer numbers: '))
+first_list = [0] * len_of_first_list
+len_of_second_list = int(input('Enter length of second set of integer numbers: '))
+second_list = [0] * len_of_first_list
+for i in range(len_of_first_list):
+    first_list[i] = int(input(f'Enter {i+1} element of first set: '))
+first_set = set(first_list)
+for i in range(len_of_second_list):
+    second_list[i] = int(input(f'Enter {i+1} element of second set: '))
+second_set = set(second_list)
+result_set = first_set.intersection(second_set)
+print(f'Result sorted set: {sorted(result_set)}.')
